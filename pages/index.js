6ac029cded88313707dfb74a1a4146febc09639f
xinluh <xinlu.h@gmail.com>
2022-03-31 11:36:57 -0600

    misc tweaks (#2)

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import GoogleMap from "../components/map";
 import { differenceInHours, formatRelative } from "date-fns";
 
 const UPDATE_PAGE_SIZE = 10;
+
 export default function Home() {
   const { data, loading, error } = useGarminLogData();
   const [numUpdateShown, setNumUpdateShown] = useState(UPDATE_PAGE_SIZE);
@@ -16,7 +17,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Xinlu's PCT Page for friends & family</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
@@ -203,46 +204,6 @@ export default function Home() {
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
         }
 
         .logo {
